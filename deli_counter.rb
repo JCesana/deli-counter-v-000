69357130f1c5f73b43b9929f31e5aa53ede14365
Josh Cesana <joshuacesana@gmail.com>
2018-05-18 00:39:17 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,7 +2,9 @@
 katz_deli = [] 
 
 def line(people)
-  if people.count == 0 { puts "The line is currently empty." }
+  if people.count == 0 
+    puts "The line is currently empty."
+  end 
 end 
 
 
